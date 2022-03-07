4f9b06603f10cfbca03751975ba82a45602c1684
Adrian <adrian_fiedler@msn.com>
2022-03-07 23:54:54 +0100

    matrix dot product

--- matrix.js
+++ matrix.js
@@ -1,42 +1,62 @@
 class Matrix {
-    constructor(rows, cols) {
-      this.rows = rows;
-      this.cols = cols;
-      this.matrix = [];
-  
+  constructor(rows, cols) {
+    this.rows = rows;
+    this.cols = cols;
+    this.matrix = [];
+
+    for (let i = 0; i < this.rows; i++) {
+      this.matrix[i] = [];
+      for (let j = 0; j < this.cols; j++) {
+        this.matrix[i][j] = 0;
+      }
+    }
+  }
+
+  randomize() {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.matrix[i][j] = Math.floor(Math.random());
+      }
+    }
+  }
+
+  add(n) {
+    if (n instanceof Matrix) {
       for (let i = 0; i < this.rows; i++) {
-        this.matrix[i] = [];
         for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] = 0;
+          this.matrix[i][j] += n.matrix[i][j];
         }
       }
-    }
-  
-    randomize() {
+    } else {
       for (let i = 0; i < this.rows; i++) {
         for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] = Math.floor(Math.random());
+          this.matrix[i][j] += n;
         }
       }
     }
-  
-    add(n) {
-      if (n instanceof Matrix) {
-        for (let i = 0; i < this.rows; i++) {
-          for (let j = 0; j < this.cols; j++) {
-            this.matrix[i][j] += n.matrix[i][j];
+  }
+
+  multiply(n) {
+    if (n instanceof Matrix) {
+      // Matrix product
+      if (this.cols !== n.rows) {
+        throw Error("Colums of A must match rows of B");
+      }
+      let a = this;
+      let b = n;
+      let result = new Matrix(a.rows, b.cols);
+      for (let i = 0; i < result.rows; i++) {
+        for (let j = 0; j < result.cols; j++) {
+          let sum = 0;
+          for (let k = 0; k < a.cols; k++) {
+            sum += a.matrix[i][k] * b.matrix[k][j];
           }
-        }
-      } else {
-        for (let i = 0; i < this.rows; i++) {
-          for (let j = 0; j < this.cols; j++) {
-            this.matrix[i][j] += n;
-          }
+          result.matrix[i][j] = sum;
         }
       }
-    }
-  
-    multiply(n) {
+      return result;
+    } else {
+      // Scalar product
       for (let i = 0; i < this.rows; i++) {
         for (let j = 0; j < this.cols; j++) {
           this.matrix[i][j] *= n;
@@ -44,4 +64,4 @@ class Matrix {
       }
     }
   }
-  
+}
