d71e7c25c112cc815faed4cc20df7ab5ae237450
Adrian Fiedler <adrian.fiedler@capgemini.com>
2022-03-08 16:08:47 +0100

    improvements

--- matrix.js
+++ matrix.js
@@ -2,12 +2,12 @@ class Matrix {
   constructor(rows, cols) {
     this.rows = rows;
     this.cols = cols;
-    this.matrix = [];
+    this.data = [];
 
     for (let i = 0; i < this.rows; i++) {
-      this.matrix[i] = [];
+      this.data[i] = [];
       for (let j = 0; j < this.cols; j++) {
-        this.matrix[i][j] = 0;
+        this.data[i][j] = 0;
       }
     }
   }
@@ -15,7 +15,7 @@ class Matrix {
   randomize() {
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
-        this.matrix[i][j] = Math.floor(Math.random());
+        this.data[i][j] = Math.floor(Math.random() * 10);
       }
     }
   }
@@ -24,43 +24,64 @@ class Matrix {
     if (n instanceof Matrix) {
       for (let i = 0; i < this.rows; i++) {
         for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] += n.matrix[i][j];
+          this.data[i][j] += n.data[i][j];
         }
       }
     } else {
       for (let i = 0; i < this.rows; i++) {
         for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] += n;
+          this.data[i][j] += n;
         }
       }
     }
   }
 
+  static multiply(a, b) {
+    // Matrix product
+    if (a.cols !== b.rows) {
+      throw Error("Colums of A must match rows of B");
+    }
+    let result = new Matrix(a.rows, b.cols);
+    for (let i = 0; i < result.rows; i++) {
+      for (let j = 0; j < result.cols; j++) {
+        let sum = 0;
+        for (let k = 0; k < a.cols; k++) {
+          sum += a.data[i][k] * b.data[k][j];
+        }
+        result.data[i][j] = sum;
+      }
+    }
+    return result;
+  }
+
   multiply(n) {
-    if (n instanceof Matrix) {
-      // Matrix product
-      if (this.cols !== n.rows) {
-        throw Error("Colums of A must match rows of B");
+    // Scalar product
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.data[i][j] *= n;
       }
-      let a = this;
-      let b = n;
-      let result = new Matrix(a.rows, b.cols);
-      for (let i = 0; i < result.rows; i++) {
-        for (let j = 0; j < result.cols; j++) {
-          let sum = 0;
-          for (let k = 0; k < a.cols; k++) {
-            sum += a.matrix[i][k] * b.matrix[k][j];
-          }
-          result.matrix[i][j] = sum;
-        }
+    }
+  }
+
+  transpose() {
+    let result = new Matrix(this.cols, this.rows);
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        result.data[j][i] = this.data[i][j];
       }
-      return result;
-    } else {
-      // Scalar product
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] *= n;
-        }
+    }
+    return result;
+  }
+
+  print() {
+    console.table(this.data);
+  }
+
+  map(func) {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let val = this.data[i][j];
+        this.data[i][j] = func(val);
       }
     }
   }
