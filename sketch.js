40fb366ec1c9bde203b8c02c732fe9bbde1c9065
Adrian <adrian_fiedler@msn.com>
2022-03-17 00:20:25 +0100

    tensorflow JS

--- sketch.js
+++ sketch.js
@@ -1,44 +1,108 @@
-let nn = new NeuralNetwork(2, 4, 1);
+let nn = getModel();
 let lr_slider;
+let inputs = [];
 
-let training_data = [
-  { inputs: [1, 0], targets: [1] },
-  { inputs: [0, 1], targets: [1] },
-  { inputs: [1, 1], targets: [0] },
-  { inputs: [0, 0], targets: [0] },
-];
+let resolution = 20;
+let cols;
+let rows;
+let xs;
 
-function setup() {
+const train_xs = tf.tensor2d([
+  [0, 0],
+  [1, 0],
+  [0, 1],
+  [1, 1],
+]);
+const train_ys = tf.tensor2d([[0], [1], [1], [0]]);
+let fps;
+
+async function setup() {
   createCanvas(400, 400);
   lr_slider = createSlider(0.01, 0.5, 0.1, 0.01);
-}
+  cols = width / resolution;
+  rows = height / resolution;
 
-function draw() {
-  background(0);
-
-  for (let i = 0; i < 1000; i++) {
-    let data = random(training_data);
-    nn.train(data.inputs, data.targets);
-  }
-
-  nn.setLearningRate(lr_slider.value());
-
-  let resolution = 10;
-  let cols = width / resolution;
-  let rows = height / resolution;
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       let x1 = i / cols;
       let x2 = j / rows;
-      let inputs = [x1, x2];
-      let y = nn.predict(inputs);
-      fill(y * 255);
-      noStroke();
-      rect(i * resolution, j * resolution, resolution, resolution);
+      inputs.push([x1, x2]);
     }
   }
-  console.log(nn.predict([1, 0]));
-  // console.log(nn.predict([0, 1]));
-  // console.log(nn.predict([1, 1]));
-  // console.log(nn.predict([0, 0]));
+
+  xs = tf.tensor2d(inputs);
+  setTimeout(train, 10);
+  fps = createSpan("FPS: " + frameRate());
+  fps.position(10, 420);
+}
+
+function getModel() {
+  const model = tf.sequential();
+
+  model.add(
+    tf.layers.dense({
+      units: 3, // number of nodes
+      activation: "sigmoid", // activation function
+      inputShape: [2], // input shape
+    })
+  );
+  model.add(
+    tf.layers.dense({
+      units: 1,
+      // input shape is inferred from previous layer
+      activation: "sigmoid",
+    })
+  );
+
+  // use stochastical gradient descent
+  const sgdOpt = tf.train.adam(0.1);
+  const config = {
+    optimizer: sgdOpt,
+    loss: "meanSquaredError",
+  };
+  model.compile(config);
+  return model;
+}
+
+function train() {
+  trainModel().then((result) => {
+    // console.log(result.history.loss[0]);
+    setTimeout(train, 10);
+  });
+}
+
+function trainModel() {
+  return nn.fit(train_xs, train_ys, {
+    shuffle: true,
+    epochs: 10,
+  });
+}
+
+function draw() {
+  background(0);
+  fps.html("FPS: " + nf(frameRate(), 1, 1));
+  
+
+  tf.tidy(() => {
+    let ys = nn.predict(xs);
+    let y_values = ys.dataSync();
+
+    let index = 0;
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        let br = y_values[index] * 255;
+        fill(br);
+        rect(i * resolution, j * resolution, resolution, resolution);
+        fill(255 - br);
+        textSize(8);
+        textAlign(CENTER, CENTER);
+        text(
+          nf(y_values[index], 1, 2),
+          i * resolution + resolution / 2,
+          j * resolution + resolution / 2
+        );
+        index++;
+      }
+    }
+  });
 }
